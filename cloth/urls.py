1eb9960d4bcf1a9aa3a75b6d646f744f308b1133
leeyongjoo <2yongjooo@gmail.com>
2020-02-27 00:38:07 +0900

    category 필터 구현 (generic view로 다시 구현할것)

--- cloth/urls.py
+++ cloth/urls.py
@@ -1,12 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 from rest_framework import routers
 
 # router = routers.DefaultRouter()
-# router.register('',)
+# router.register('alls', views.AllViewSet)
+# router.register('outer', views.OuterViewSet)
+
 
 urlpatterns = [
+    # path('', include(router.urls)),
     path('', views.ListCloth.as_view()),
     path('<int:pk>/', views.DetailCloth.as_view()),
+    path('<str:category>/', views.ListCategory.as_view()),
+    path('<str:category>/<int:pk>/', views.DetailCategory.as_view()),
 ]
--- cloth/views.py
+++ cloth/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics
+from rest_framework import generics, views, status
+from rest_framework.response import Response
+from django.http import Http404
 
 from .models import Cloth
 from .serializers import ClothSerializer
+
+cgDict = {
+    'outer': 1,
+    'top' : 2,
+
+}
 
 class ListCloth(generics.ListCreateAPIView):
     queryset = Cloth.objects.all()
@@ -13,3 +21,30 @@ class ListCloth(generics.ListCreateAPIView):
 class DetailCloth(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cloth.objects.all()
     serializer_class = ClothSerializer
+
+
+class ListCategory(views.APIView):
+    def get(self, request, category, format=None):
+        queryset = Cloth.objects.filter(category=cgDict.get(category, ''))
+        serializer = ClothSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class DetailCategory(views.APIView):
+    def get_object(self, pk):
+        try:
+            return Cloth.objects.get(pk=pk)
+        except Cloth.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category, pk):
+        cloth = self.get_object(pk)
+        serializer = ClothSerializer(cloth)
+        return Response(serializer.data)
+
+    def put(self, request, category, pk, format=None):
+        cloth = self.get_object(pk)
+        serializer = ClothSerializer(cloth, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
