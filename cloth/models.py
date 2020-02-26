c661daf78df0fcb7696e3e5e57e87873affe2481
leeyongjoo <2yongjooo@gmail.com>
2020-02-26 17:11:29 +0900

    AutoDateTiemField 추가

--- cloth/models.py
+++ cloth/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.utils import timezone
 
+class AutoDateTimeField(models.DateTimeField):
+    def pre_save(self, model_instance, add):
+        return timezone.now()
+
 # Create your models here.
 class Cloth(models.Model):
     # id = models.IntegerField(primary_key=True)
@@ -10,7 +14,7 @@ class Cloth(models.Model):
     price = models.CharField(max_length=100)
     category = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True, default=timezone.now, editable=False)
-    modified = models.DateTimeField(auto_now=True, default=timezone.now, editable=False)
+    modified = models.AutoDateTimeField(auto_now=True, default=timezone.now, editable=False)
 
 
     def __str__(self):
