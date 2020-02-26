3c2b46f6a4cbd20b28f0edea31c6a52949b1f207
leeyongjoo <2yongjooo@gmail.com>
2020-02-27 00:35:54 +0900

    api --> cloth 변경

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from rest_framework import routers
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('cloth.urls')),
-
+    path('cloth/', include('cloth.urls')),
 ]
