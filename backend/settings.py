2114c33b5b8340faabcd28824519ca6c28e5b793
leeyongjoo <2yongjooo@gmail.com>
2020-02-27 19:32:01 +0900

    mysql db 설정

--- backend/settings.py
+++ backend/settings.py
@@ -99,10 +99,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'squaremall',
+        'USER': 'root',
+        'PASSWORD': '82134',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
