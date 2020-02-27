2f37523940c5852afe5d58e4ec8a52b63f644fec
leeyongjoo <2yongjooo@gmail.com>
2020-02-27 19:32:35 +0900

    description, pageUrl, create, modified 추가 / clothImg 이름 변경

--- cloth/models.py
+++ cloth/models.py
@@ -6,19 +6,20 @@ class Cloth(models.Model):
     # id = models.IntegerField(primary_key=True)
     brand = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
-    date = models.CharField(max_length=100, null=True) # 임시
-    clothImg = models.URLField()
+    description = models.TextField(default='상품 설명', blank=True, null=True)
+    clothImgUrl = models.URLField()
+    pageUrl = models.URLField()
     price = models.CharField(max_length=100)
     category = models.IntegerField()
 
-    # created = models.DateTimeField(editable=False)
-    # modified = models.DateTimeField(editable=False)
-    # def save(self, *args, **kwargs):
-    #     ''' On save, update timestamps '''
-    #     if not self.id:
-    #         self.created = timezone.now()
-    #     self.modified = timezone.now()
-    #     return super(Cloth, self).save(*args, **kwargs)
+    created = models.DateTimeField(editable=False)
+    modified = models.DateTimeField(editable=False)
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Cloth, self).save(*args, **kwargs)
 
     def __str__(self):
         """A string representation of the model."""
