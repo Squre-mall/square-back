28773071aadeec1b4e3841393d2064a0d8987ffb
leeyongjoo <2yongjooo@gmail.com>
2020-02-29 12:22:42 +0900

    fields 리스트에서 튜플로 변경

--- cloth/serializers.py
+++ cloth/serializers.py
@@ -3,5 +3,5 @@ from .models import *
 
 class ClothSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ['id'] + [a.name for a in Cloth._meta.get_fields()]
+        fields = ('id',) + tuple(a.name for a in Cloth._meta.get_fields())
         model = Cloth
