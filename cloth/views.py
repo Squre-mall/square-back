6b7ce7d414fba84f543b4a2c988115095948c6ee
leeyongjoo <2yongjooo@gmail.com>
2020-03-01 18:31:56 +0900

    JSONRenderer, BrowsableAPIRenderer 추가

--- cloth/views.py
+++ cloth/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import generics, views, status
 from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from django.http import Http404
 
 from .models import Cloth
@@ -19,6 +20,7 @@ cgDict = {
 class ListCloth(generics.ListCreateAPIView):
     queryset = Cloth.objects.all()
     serializer_class = ClothSerializer
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ClothSerializer.Meta.fields
     search_fields = ClothSerializer.Meta.fields
