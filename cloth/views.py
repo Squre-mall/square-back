f2f15aa321679ecc16682fa313ce387ca2062240
leeyongjoo <2yongjooo@gmail.com>
2020-03-02 12:48:29 +0900

    ListCategory class 삭제

--- cloth/views.py
+++ cloth/views.py
@@ -11,12 +11,6 @@ from .serializers import ClothSerializer
 
 from django_filters.rest_framework import DjangoFilterBackend
 
-cgDict = {
-    'outer': 1,
-    'top' : 2,
-    'bottom':3,
-}
-
 class ListCloth(generics.ListCreateAPIView):
     queryset = Cloth.objects.all()
     serializer_class = ClothSerializer
@@ -30,29 +24,3 @@ class DetailCloth(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cloth.objects.all()
     serializer_class = ClothSerializer
 
-
-class ListCategory(views.APIView):
-    def get(self, request, category, format=None):
-        queryset = Cloth.objects.filter(category=cgDict.get(category, ''))
-        serializer = ClothSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-class DetailCategory(views.APIView):
-    def get_object(self, pk):
-        try:
-            return Cloth.objects.get(pk=pk)
-        except Cloth.DoesNotExist:
-            raise Http404
-
-    def get(self, request, category, pk):
-        cloth = self.get_object(pk)
-        serializer = ClothSerializer(cloth)
-        return Response(serializer.data)
-
-    def put(self, request, category, pk, format=None):
-        cloth = self.get_object(pk)
-        serializer = ClothSerializer(cloth, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
